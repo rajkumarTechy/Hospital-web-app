5891b0102f52481754ea81103378623d664fc6bc
rajkumar-1823 <raj1823mechons@gmail.com>
2024-03-26 20:11:06 +0530

    new change

--- server/index.js
+++ server/index.js
@@ -22,13 +22,13 @@ app.use(cors({
     credentials: true
 }));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://hospital-web-app.vercel.app');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://hospital-web-app.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 const db = mysql.createConnection({
     host: process.env.HOST_NAME,
