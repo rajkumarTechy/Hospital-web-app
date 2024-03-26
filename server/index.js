1e96a2e637309de1abfc140b54b0f92fe290bb64
rajkumar-1823 <raj1823mechons@gmail.com>
2024-03-26 20:56:20 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ["https://hospital-web-app.vercel.app/"],
+    origin: ["*"],
     methods: ["POST", "GET"],
     credentials: true
 }));
